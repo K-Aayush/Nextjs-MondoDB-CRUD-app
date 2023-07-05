8c29954e1b4bcfd1be654dccf59bc1556ce71f1b
Aayush <ayushkarkee6@gmail.com>
2023-07-05 17:51:21 +0545

    creating edit session

---
+++ app/editTopic/[id]/page.jsx
@@ -0,0 +1,12 @@
+import EditTopicForm from '@/components/editTopicForm';
+import React from 'react'
+
+const EditTopic = () => {
+  return (
+    <div>
+        <EditTopicForm />
+    </div>
+  )
+}
+
+export default EditTopic;
